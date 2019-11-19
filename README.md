41598e8f331ab69346dc70c57987493d18e2c2df
jacobhsu <jacob.hsu.tw@gmail.com>
2019-11-19 16:26:30 +0800

    rajithaeyee/ReactTodoChromeExtention

--- README.md
+++ README.md
@@ -4,6 +4,35 @@
 
 `npx create-react-app react-ext`
 
+# npm 
+
+`react-cookie`  
+
+# note 
+
+使用PropTypes 进行类型检查– React 中文文档v16.6.3
+
+Component `import React, { Component } from 'react';`
+
+
+Refused to execute inline script because it violates the following Content Security Policy directive: "script-src 'self'". Either the 'unsafe-inline' keyword, a hash ('sha256-P/KdvuJkHQwRCjP3/zvhW4AjD2Jo5r7K5Ra5LETfSAk='), or a nonce ('nonce-...') is required to enable inline execution.
+
+把 key 加進 content_security_policy  
+```js
+{
+  "name": "Chrome Todo",
+    "version": "1.0",
+    "manifest_version": 2,
+    "browser_action": {
+        "default_popup": "index.html"
+    },
+    "content_security_policy": "script-src 'self' 'sha256-P/KdvuJkHQwRCjP3/zvhW4AjD2Jo5r7K5Ra5LETfSAk='; object-src 'self'"
+}
+```
+
 # Referneces
 
 [How to build a Chrome Extension using your React App](https://medium.com/@rajithaeye/how-to-build-a-chrome-extension-using-your-reactjs-app-3655dc08fd16)  
+[Quickstart a Chrome Extension with create-react-app](https://www.lsenta.io/posts/chrome-extension-react-quickstart/)  
+
+[Web 安全之 CSP（Content Security Policy）](https://kknews.cc/code/abx9ya6.html)  
--- react-ext/src/App.js
+++ react-ext/src/App.js
@@ -1,26 +1,118 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Components/Todo/Todo';
+import TodoAdder from './Components/TodoAdder/TodoAdder';
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    const { cookies } = props;
+    this.state = {
+      todods:cookies.get('tasks')?cookies.get('tasks'):[],
+      taskName:""
+      };
+  }
+  
+  completeTask=(event,index)=>{
+    const { cookies } = this.props;
+    const todos =[...this.state.todods];
+    todos[index].checked = "checked";
+    const tasksList=JSON.stringify(todos);
+    cookies.set('tasks',tasksList);
+    this.setState({
+      todods: todos
+    });
+  }
+
+  addTodo = (taskName)=>{
+    const { cookies } = this.props;
+    
+    const todos = [...this.state.todods];
+    if(this.state.taskName){
+      console.log(cookies.get('tasks'));
+      todos.unshift({task:taskName, checked:""});
+      const tasksList=JSON.stringify(todos);
+        cookies.set('tasks',tasksList);
+      //console.log( stringArr);
+      this.setState({
+        todods: todos
+      });
+    this.setState({
+      taskName: ""
+    });
+    }
+  }
+
+  changeTaskName = (event) =>{
+    const task = event.target.value;
+    this.setState({
+      taskName: task
+    });
+  }
+
+  keyPressed(event){
+    if(event.key==='Enter'){
+      this.addTodo(this.state.taskName);
+    }
+  }
+
+  deleteTask(event, index){
+    const { cookies } = this.props;
+    const todos =[...this.state.todods];
+    todos.splice(index, 1);
+
+    const tasksList=JSON.stringify(todos);
+    cookies.set('tasks',tasksList);
+
+    this.setState({
+      todods: todos
+    });
+  }
+
+  render() {
+    let myTodos = this.state.todods.map((t,index)=>{
+        return (<Todo task={t.task} 
+          click={(event)=>{this.completeTask(event,index)}}
+          checked={t.checked}
+          delete={(event) => {this.deleteTask(event, index)}} />);
+    });
+    return (
+      <div className="App">
+        <TodoAdder change={(event)=>{this.changeTaskName(event)}} taskName={this.state.taskName} click={(event)=>{this.addTodo(this.state.taskName)}} enterPressed={(event)=>this.keyPressed(event)}/>
+        <ul>
+          {myTodos}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default withCookies(App);
